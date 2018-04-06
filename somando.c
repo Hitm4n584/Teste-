f9bbf897ad79b1b40a27be4197add671d1495815
Hitm4n584 <33663422+Hitm4n584@users.noreply.github.com>
2018-04-06 13:54:26 -0300

    Update somando.c

--- somando.c
+++ somando.c
@@ -3,7 +3,13 @@ Aulão dos veteranos
 #include <stdlib.h>
 #include <stdio.h>
 
+int soma(int main)
 int main(){
 
+return 
+}
 
+int soma(int num){
+if(numero==1)return 1;
+return numero + soma(numero + 1);
 }
